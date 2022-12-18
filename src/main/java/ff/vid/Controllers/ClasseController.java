2240050b40f0caafc066585d952f3021f724712e
adnane moutaouakil <adn.moutaouakil@gmail.com>
2022-12-18 10:31:30 +0100

    decodage d object jasonNode into Classe class and save it

--- src/main/java/ff/vid/Controllers/ClasseController.java
+++ src/main/java/ff/vid/Controllers/ClasseController.java
@@ -3,9 +3,17 @@ package ff.vid.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ff.vid.Repositories.ClasseRepository;
 import ff.vid.entities.Classe;
@@ -22,6 +30,22 @@ public class ClasseController {
 	public List<Classe> getAllClasses(){
 		return cr.findAll();
 	}
+	
+
+	@PostMapping("/addClasse")
+	public void AjouterClasse(@RequestBody JsonNode cls) {     
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Classe c = mapper.readValue(cls.toString(), Classe.class);
+			cr.save(c);		
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+	
+	
 	
 	
 }
